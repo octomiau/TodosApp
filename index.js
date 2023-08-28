812b6f612e59b1a69c94de5218736b2d0f23f472
Diana <avilesdianaa@gmail.com>
2023-08-27 21:12:19 -0600

    input creation

--- index.js
+++ index.js
@@ -13,9 +13,26 @@ function App(){
             isCompleted: false,
         }
     ]);
+    const [value, setValue]= React.useState('');
+    const handleSubmit= e => {
+        e.preventDefault();
+        if(!value) return;
+        const newTodos = [...todos, {text:value, is complited:false}]
+        setTodos(newTodos);
+        setValue('');
+    }
     return (<>
         {todos.map((todo,i) => 
         <div classname="todo" key={i}> {todo.text}</div>)}
+        <formonDubmit={handleSubmit}>
+            <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo ..."
+            onChange={e=> setValue(e.target.value)}
+            />
+        </form>
     </>);
 }
 
